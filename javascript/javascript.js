ce54ff203f31c0b1171052610ec193a9228b50a6
Arvin Mahmoudi <mahmoudi.arvin@gmail.com>
2019-12-17 18:23:48 -0500

    WIP on JS file

---
+++ javascript/javascript.js
@@ -0,0 +1,16 @@
+var userInput =prompt("How Many Character Your Password Wants To Be ? (Between 8 to 28 character) ");
+
+function passLength(){
+
+while (userInput>=10 | userInput<=28){
+    alert( "You Must Choose Number Between 8 to 28");
+    prompt("How Many Character Your Password Wants To Be ? (Between 8 to 28 character) ")
+};
+console.log("this works");
+}
+passLength();
+
+
+ 
+ var arr =["A","B","C","D","E","F","G","H","I","J","K","L","M","N","O","P","Q","R","S","T","U","V","W","X","Y","Z","a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z","!","#","$","%","&","'","(",")","*","+","-",".","/",":",";","<","=",">","?","@","[","]","^","_","`","{","|","}","~"];
+
